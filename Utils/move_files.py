ee5ef65934a1d11083d8359b623ee96d34282c96
niejeuri <niejeuri@uni-bremen.de>
2018-08-04 17:05:23 +0200

    added more labeled images

---
+++ Utils/move_files.py
@@ -0,0 +1,8 @@
+import os
+dir = "/home/niels/Repositories/Images/NeuralNets/RoadMarkingsDetection/data/training/withoutfog/"
+dest = "/home/niels/Repositories/bachelorarbeit/RoadMarkingsDetection/data/training/streetmarkings/"
+for file in os.listdir(dir):
+	if file.endswith(".xml"):
+		os.rename(os.path.join(dir, file), os.path.join(dest, file))
+		os.rename(os.path.splitext(os.path.join(dir, file))[0] + ".png", os.path.splitext(os.path.join(dest, file))[0] + ".png")
+		print(os.path.splitext(os.path.join(dir, file))[0])
